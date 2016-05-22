e88ec5513bbd1a961657a2e75591b8d9056270c4
David G <david.a.grs@gmail.com>
2016-05-22 19:31:14 +0200

    Adding hot_set to benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,9 +1,12 @@
 #include "futils.h"
+#include "hot_set.h"
 
 #include <geiger/geiger.h>
 
 #include <map>
 #include <unordered_map>
+#include <google/dense_hash_map>
+
 #include <cxxabi.h>
 
 const std::vector<std::string>& get_dict_words()
@@ -20,31 +23,39 @@ const std::vector<std::string>& get_dict_words()
     return v;
 }
 
-template <typename _MapT, typename _KeyT>
-_MapT prepare_map(const std::vector<_KeyT>& values)
+template <typename _MapT>
+_MapT prepare_map()
 {
     _MapT m;
-
-    for (const auto& v : values)
+    for (const auto& v : get_dict_words())
         m.insert(std::make_pair(v, 0));
+    return m;
+}
+
+template <>
+hov_set<std::string> prepare_map<hov_set<std::string>>()
+{
+    hov_set<std::string> m;
+    for (const auto& v : get_dict_words())
+        m.insert(v);
     return m;
 }
 
 template <typename _MapT, typename... Args>
 void add_to_benchmark(geiger::suite<Args...>& s)
 {
-    const auto& values = get_dict_words();
-    auto m = prepare_map<_MapT>(values);
+    auto m = prepare_map<_MapT>();
 
     char* test_name = __cxxabiv1::__cxa_demangle(typeid(_MapT).name(), nullptr, nullptr, nullptr);
-    s.add(test_name, [&values, m = std::move(m)]()
+    s.add(test_name, [m = std::move(m)]()
     {
+        const auto& values = get_dict_words();
         int sum = 0;
         for (const auto& v : values)
         {
             auto it = m.find(v);
             assert(it != m.end());
-            sum += it->second;
+            sum += m.size();
         }
     });
     free(test_name);
@@ -58,6 +69,8 @@ int main()
 
     add_to_benchmark<std::map<std::string, int>>(s);
     add_to_benchmark<std::unordered_map<std::string, int>>(s);
+    //add_to_benchmark<google::dense_hash_map<std::string, int>>(s);
+    add_to_benchmark<hov_set<std::string>>(s);
 
     s.run();
 
